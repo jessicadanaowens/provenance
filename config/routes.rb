ccdc651bb55ca1d7b21964e0b07e2936714d22bf
Jessica Owens <jessicadanaowens@gmail.com>
2015-12-12 22:42:27 -0700

    Initialized new project -user can sign up, log in, and log out -initialized welcome page and dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get 'sessions/new'
+
+  root 'welcome#index'
+
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
+  get 'dashboard' => 'dashboard#index'
+
+  resources :users
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
